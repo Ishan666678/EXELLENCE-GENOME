82d7834de942cd3295ddd3fb08f20409666c64a2
Ishan666678 <74366197+Ishan666678@users.noreply.github.com>
2021-01-03 20:25:41 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff1da3a7dc2c5001e034e74",
+				"5ff1da3a7dc2c5001e034e76"
+			]
+		},
+		"5ff1da3a7dc2c5001e034e74": {
+			"id": "5ff1da3a7dc2c5001e034e74",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff1da3a7dc2c5001e034e76": {
+			"id": "5ff1da3a7dc2c5001e034e76",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
